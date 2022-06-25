6be8a693783be85bc4948cd8f950267518409ef1
Evhenii Ashytok <judginash@gmail.com>
2022-06-25 20:49:51 +0300

    1.6 custom argument

---
+++ Training/TestOM/Model/ManagerCustomImplementation.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\TestOM\Model;
+class ManagerCustomImplementation implements ManagerInterface
+{
+    public function set()
+    {
+// custom logic
+    }
+
+    public function get()
+    {
+// custom logic
+    }
+}
