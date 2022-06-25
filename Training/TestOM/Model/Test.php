2eed7bab1c498218602c78b73b5262b787a17071
Evhenii Ashytok <judginash@gmail.com>
2022-06-25 20:40:45 +0300

    1.5 Factory

--- Training/TestOM/Model/Test.php
+++ Training/TestOM/Model/Test.php
@@ -11,18 +11,21 @@ class Test
     private $arrayList;
     private $name;
     private $number;
+    private $managerFactory;
 
     public function __construct(
         \Training\TestOM\Model\ManagerInterface $manager,
         $name,
         int $number,
-        array $arrayList
+        array $arrayList,
+        \Training\TestOM\Model\ManagerInterfaceFactory $managerFactory
     )
     {
         $this->manager = $manager;
         $this->name = $name;
         $this->number = $number;
         $this->arrayList = $arrayList;
+        $this->managerFactory = $managerFactory;
     }
 
 
@@ -35,5 +38,10 @@ class Test
         print_r($this->number);
         echo '<br>';
         print_r($this->arrayList);
+
+        echo '<br>';
+
+        $newManager = $this->managerFactory;
+        print_r(get_class($newManager));
     }
 }
