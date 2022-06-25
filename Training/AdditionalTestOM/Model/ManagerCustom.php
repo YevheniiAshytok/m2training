0734727585b7babb4f620b29dd5c00a082cf8c9a
Evhenii Ashytok <judginash@gmail.com>
2022-06-25 14:18:15 +0300

    1.3 AdditionalTestOM

---
+++ Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\TestOM\Model;
+class ManagerCustom implements ManagerInterface
+{
+    public function set()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+}
