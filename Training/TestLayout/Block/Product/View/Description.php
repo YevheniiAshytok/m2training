c18e45aa9f3c3b9b73e62cec9aca991bd6c3fdd2
Evhenii Ashytok <judginash@gmail.com>
2022-07-14 19:13:54 +0300

    3.4

---
+++ Training/TestLayout/Block/Product/View/Description.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\TestLayout\Block\Product\View;
+
+class Description
+{
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+        $subject->getProduct()->setDescription('Test description');
+    }
+}
