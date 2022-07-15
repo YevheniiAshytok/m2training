99796049ec733b7f52cc111a9842ea40db8571e5
Evhenii Ashytok <judginash@gmail.com>
2022-07-15 20:59:27 +0300

    4.2

---
+++ Training/Rendering/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/catalog-add-to-cart': {
+                'Training_Rendering/js/catalog-add-to-cart-mixin': true
+            }
+        }
+    }
+};
